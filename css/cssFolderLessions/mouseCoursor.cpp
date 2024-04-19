e1d1c99c4cb6473b67e9c0e258dcd501454b512d
mdelhajaji2000 <149912377+mdelhajaji2000@users.noreply.github.com>
2024-04-19 21:35:39 +0100

    Update..

---
+++ css/cssFolderLessions/mouseCoursor.cpp
@@ -0,0 +1,15 @@
+button {
+  background: transparent;
+  border: none;
+  color: red;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+span {
+  cursor: -webkit-grab;
+}
+
+p {
+  cursor: crosshair;
+}
